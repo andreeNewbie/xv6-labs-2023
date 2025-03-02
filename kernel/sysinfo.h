fa7e4c2fd43636cd47094e44485df5c7a2c88983
Tramie_Nguyen <myn897676@gmail.com>
2025-03-02 22:39:10 +0700

    committing files

--- kernel/sysinfo.h
+++ kernel/sysinfo.h
@@ -1,4 +1,11 @@
-struct sysinfo {
-  uint64 freemem;   // amount of free memory (bytes)
-  uint64 nproc;     // number of process
+#ifndef _SYSINFO_H_
+#define _SYSINFO_H_
+
+struct sysinfo
+{
+  uint64 freemem; // amount of free memory (bytes)
+  uint64 nproc;   // number of process
+  uint64 load_avg;
 };
+
+#endif
--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -5,6 +5,11 @@
 #include "memlayout.h"
 #include "spinlock.h"
 #include "proc.h"
+#include "sysinfo.h"
+
+extern uint64 get_freemem(void); // return number of freemem byte
+extern uint64 get_nproc(void);
+extern uint64 get_loadavg(void);
 
 uint64
 sys_exit(void)
@@ -12,7 +17,7 @@ sys_exit(void)
   int n;
   argint(0, &n);
   exit(n);
-  return 0;  // not reached
+  return 0; // not reached
 }
 
 uint64
@@ -43,7 +48,7 @@ sys_sbrk(void)
 
   argint(0, &n);
   addr = myproc()->sz;
-  if(growproc(n) < 0)
+  if (growproc(n) < 0)
     return -1;
   return addr;
 }
@@ -55,12 +60,14 @@ sys_sleep(void)
   uint ticks0;
 
   argint(0, &n);
-  if(n < 0)
+  if (n < 0)
     n = 0;
   acquire(&tickslock);
   ticks0 = ticks;
-  while(ticks - ticks0 < n){
-    if(killed(myproc())){
+  while (ticks - ticks0 < n)
+  {
+    if (killed(myproc()))
+    {
       release(&tickslock);
       return -1;
     }
@@ -90,4 +97,23 @@ sys_uptime(void)
   xticks = ticks;
   release(&tickslock);
   return xticks;
+}
+
+uint64 sys_sysinfo(void)
+{
+  uint64 addr;
+  struct sysinfo info;
+  // get address of struct sysinfo from userspace
+  argaddr(0, &addr);
+
+  // get system information
+  info.freemem = get_freemem();
+  info.nproc = get_nproc();
+  info.load_avg = get_loadavg();
+
+  printf("Load average: %d\n", info.load_avg);
+  // copy result to user space
+  if (copyout(myproc()->pagetable, addr, (char *)&info, sizeof(info)) < 0)
+    return -1;
+  return 0;
 }
