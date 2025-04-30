000bd298d73214ae338fe62e883c1cf6c6b92aa6
Nhut Anh <andree.forwork@gmail.com>
2025-04-30 20:41:29 +0700

    Bo sung noi dung phan lab 02

--- user/trace.c
+++ user/trace.c
@@ -8,18 +8,21 @@ main(int argc, char *argv[])
 {
     int i;
     char *nargv[MAXARG];
-  
-    if(argc < 3 || (argv[1][0] < '0' || argv[1][0] > '9')){
+
+    if(argc < 3 || (argv[1][0] < '0' || argv[1][0] > '9')) // (trace, mask, và command).
+    {
       fprintf(2, "Usage: %s mask command\n", argv[0]);
       exit(1);
     }
   
-    if (trace(atoi(argv[1])) < 0) {
+    if (trace(atoi(argv[1])) < 0) // Gắn bitmask
+    {
       fprintf(2, "%s: trace failed\n", argv[0]);
       exit(1);
     }
     
-    for(i = 2; i < argc && i < MAXARG; i++){
+    for(i = 2; i < argc && i < MAXARG; i++) //Thực hiện tiến trình yêu cầu
+    {
       nargv[i-2] = argv[i];
     }
     exec(nargv[0], nargv);
