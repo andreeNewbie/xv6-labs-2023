de51ed0c3d9726f4bca74f1b1db1e7a7598c6175
Tramie_Nguyen <myn897676@gmail.com>
2025-05-02 01:24:38 +0700

    adding commands

--- kernel/sysinfo.h
+++ kernel/sysinfo.h
@@ -5,7 +5,7 @@ struct sysinfo
 {
   uint64 freemem;  // amount of free memory (bytes)
   uint64 nproc;    // number of process
-  uint64 load_avg; // mức tại CPU
+  uint64 load_avg; // mức tải CPU
 };
 
 #endif
